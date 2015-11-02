6257f151cc941146ce4f890898435a28641ac4fa
chenalgo <chen@algomizer.com>
2015-11-02 10:51:10 +0200

    ftghgfh

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -15,7 +15,7 @@ namespace AngularWebApiApp.Controllers
         public ProductController()
         {
             if (PgaeLoadFlag == 1) //use this only for first time page load
-            {
+            { //sdfsfd
                 //Three product added to display the data
                 products.Value.Add(new Product { ID = 1, Name = "bus", Category = "Toy", Price = 200.12M });
                 products.Value.Add(new Product { ID = 2, Name = "Car", Category = "Toy", Price = 300 });
